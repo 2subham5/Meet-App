27392877dc03d82f06d8e96fdfa2b670bb807faf
Subham Kumar Sharma <subhamrelax45@gmail.com>
2024-07-19 14:17:45 +0530

    minor

--- server/index.js
+++ server/index.js
@@ -6,6 +6,21 @@ const io = new Server({
     cors: true,
 });
 const app = express();
+// const allowedOrigins = ['https://b-bbackend.vercel.app', 'https://meet-app-delta.vercel.app/','http://localhost:5173'];
+
+//   app.use(cors({
+//     origin: function (origin, callback) {
+//       if (!origin) return callback(null, true); // Allow non-browser requests
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     allowedHeaders: 'Authorization,Content-Type',
+//     credentials: true, // Allow cookies and authorization headers
+//   }));
 app.use(bodyParser.json());
 // to map a particular mail to socketId
 const emailToSocketMapping = new Map();
